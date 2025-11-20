ca07956640051f5fd68a70d3697a09daa897d93d
RSimonds <samuelrasimonds@gmail.com>
2025-11-20 14:05:30 -0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Installing Python dependencies..."
+
+if [ -f requirements.txt ]; then
+  pip install -r requirements.txt
+else
+  pip install flask gunicorn
+fi
+
+echo "Starting Gunicorn server..."
+
+# Railway sets PORT in the environment, default to 5000 if not set
+exec gunicorn server:app --bind 0.0.0.0:${PORT:-5000}
